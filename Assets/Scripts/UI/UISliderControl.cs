5441cd2f6fc0a9897850c58c4542924b85667693
lsalmon <lsalmon.UBS@gmail.com>
2021-09-20 00:46:18 +0200

    Fixed canvas moving around ball when camera moves

--- Assets/Scripts/UI/UISliderControl.cs
+++ Assets/Scripts/UI/UISliderControl.cs
@@ -5,7 +5,6 @@ public class UISliderControl : MonoBehaviour
     // Manipulate canvas rotation for slider elements on the ball so that it doesnt swing around
     private Quaternion m_CanvasRotation;
     private float m_RotationSave;
-    private Vector3 m_CanvasPosition;
     private Rigidbody m_Rigidbody;
     private Camera m_Camera;
 
@@ -13,7 +12,6 @@ public class UISliderControl : MonoBehaviour
     {
         // Local rotation at init
         m_CanvasRotation = transform.parent.localRotation;
-        m_CanvasPosition = transform.localPosition;
     }
 
     public void SetCamera(Camera camera)
@@ -28,32 +26,33 @@ public class UISliderControl : MonoBehaviour
 
     private void Update()
     {
+        // Only move slider around when ball is not moving
         if (m_Rigidbody.IsSleeping())
         {
             // Freeze rotation of parent (empty object)
             transform.parent.rotation = m_CanvasRotation;
 
+            // Get camera rotation
             Quaternion cam_rot = m_Camera.transform.rotation;
-            cam_rot = Quaternion.Inverse(cam_rot);
 
-            // Get angle between camera and ball
-            float angle_diff = (transform.rotation.eulerAngles.y - cam_rot.eulerAngles.y);
+            // Invert camera rotation
+            float angle = (cam_rot.eulerAngles.y + 180) % 360;
 
-            if (angle_diff < 5.0f && angle_diff > -5.0f)
-            {
-                Debug.Log("Aligned");
-                m_RotationSave = 0.0f;
-            }
-            else
-            {
-                if (m_RotationSave == 0.0f)
-                {
-                    m_RotationSave = angle_diff;
-                }
-            }
+            // Get direction between camera and ball
+            // then invert it and ignore Y component
+            Vector3 position = m_Rigidbody.position;
+            Vector3 pos_cam = m_Camera.transform.position;
+            Vector3 dir_canvas = -(position - pos_cam).normalized;
 
-            // Rotate slider around ball
-            transform.RotateAround(transform.parent.position, Vector3.up, m_RotationSave);
+            // Set position of slider to match the rotation
+            // (have slider behind ball at all angles)
+            position.x += dir_canvas.x * 0.4f;
+            position.z += dir_canvas.z * 0.4f;
+            transform.position = position;
+
+            // Set rotation of slider
+            Quaternion ball_rot = Quaternion.Euler(transform.rotation.eulerAngles.x, angle, transform.rotation.eulerAngles.z);
+            transform.rotation = ball_rot;
         }
     }
 }
