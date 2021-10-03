98c5528cd74b0568d386b9acf9f76af25b1bc412
lsalmon <lsalmon.UBS@gmail.com>
2021-10-04 01:24:25 +0200

    Changed text on screen, made strokes text only last 3s and edited physics to make the ball stop rolling faster

--- Assets/Scripts/UI/UIDisplay.cs
+++ Assets/Scripts/UI/UIDisplay.cs
@@ -14,20 +14,31 @@ public class UIDisplay : MonoBehaviour
 
         // Move text upwards
         RectTransform texttransform = m_Text.GetComponent<RectTransform>();
-        texttransform.anchoredPosition = new Vector2(100f, 40f);
+        texttransform.anchoredPosition = new Vector2(150f, 40f);
 
         // Change font size
         m_Text.fontSize = 35;
         texttransform.sizeDelta = new Vector2(m_Text.fontSize * 10, 100f);
     }
 
+    private IEnumerator PrintStrokes(uint strokes)
+    {
+        m_Text.text = "+"+strokes;
+        yield return new WaitForSeconds(3);
+        m_Text.text = "";
+    }
+
     public void DisplayStrokes(uint strokes)
     {
-        m_Text.text = "Strokes: "+strokes;
+        StopAllCoroutines();
+        StartCoroutine(PrintStrokes(strokes));
     }
 
     public void DisplayEnd(uint strokes)
     {
-        m_Text.text = "You Won in "+strokes+" strokes";
+        // Move text to the left
+        RectTransform texttransform = m_Text.GetComponent<RectTransform>();
+        texttransform.anchoredPosition = new Vector2(100f, 40f);
+        m_Text.text = "Your score: +"+strokes;
     }
 }
