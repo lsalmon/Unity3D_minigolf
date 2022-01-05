ffeb3560fca1d949f54c1bf6c3442f624affb13b
lsalmon <lsalmon.UBS@gmail.com>
2022-01-05 23:52:41 +0100

    Added background music and credit for audio clips in readme

--- Assets/Scripts/Sounds/PlaySounds.cs
+++ Assets/Scripts/Sounds/PlaySounds.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class PlaySounds : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    public AudioClip background;
     public AudioClip collision;
     public AudioClip completed;
 
     void Start()
     {
-        GetComponent<AudioSource>().playOnAwake = false;
+        audioSource = GetComponent<AudioSource>();
 
-        // Assign collision sound to source
-        GetComponent<AudioSource>().clip = collision;
+        // Assign background music to source
+        audioSource.clip = background;
+        audioSource.volume = 0.5f;
+
+        audioSource.Play();
     }
 
     // Play collision sound every time we hit a collider (meshes for the ground, sides, etc)
@@ -19,12 +25,13 @@ public class PlaySounds : MonoBehaviour
     // or when it flies and falls back on the ground
     void OnCollisionEnter()
     {
-        GetComponent<AudioSource>().Play();
+        // Play collision sound once then go back to background music
+        audioSource.PlayOneShot(collision);
     }
 
     public void Completed()
     {
-        GetComponent<AudioSource>().clip = completed;
-        GetComponent<AudioSource>().Play();
+        audioSource.clip = completed;
+        audioSource.Play();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Unity3D_minigolf
 Yet another Unity minigolf game
+
+## Credits
+- collision_simple.ogg -> "Collision Nutfall (Yo Frankie!)" from the Blender Foundation on opengameart.org
+- finished.mp3 -> "Completion sound." from Brandon Morris on opengameart.org
+- background_music.mp3 -> "ambient trap song (instrumental)" from Oddroom on opengameart.org
